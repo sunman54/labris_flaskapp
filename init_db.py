8b1b6bc67f12ffdae64c0fd66da99d1ff710b0f7
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2023-03-20 11:49:30 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,11 +1,9 @@
-import os
 import psycopg2
 
-
 class user_connect:
-    name= "sunman"
-    password= "labris"
-    project_name= "labris"
+    name = "sunman"
+    password = "labris"
+    project_name = "labris"
     dbConfig = (f"postgresql://{name}:{password}@localhost/{project_name}")
     # dbConfig = (f"postgresql://{name}:{password}@localhost:{port}/{project_name}")
 
@@ -16,35 +14,15 @@ class user_connect:
         password='labris')
 
     cur = conn.cursor()
-    conn.commit()
+    #conn.commit()
 
-conn =  psycopg2.connect(
-        host="localhost",
-        database="labris",
-        user= 'sunman',
-        password='labris')
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="labris",
+    user='sunman',
+    password='labris')
 
 curr = conn.cursor()
 
-"""
-cur.execute('DROP TABLE IF EXISTS users;')
-
-
-cur.execute('CREATE TABLE users (id serial PRIMARY KEY,'
-            
-                                 'username varchar (50) NOT NULL,'
-            
-                                 'firstname varchar (50) NOT NULL,'
-                                 'middlename varchar (50),'
-                                 'lastname varchar (50) NOT NULL,'
-                                 'birthdate date,'
-            
-                                 'email varchar (150) NOT NULL,'
-                                 'password varchar (50) NOT NULL,'
-                                 )"""
-# INSERT INTO users (username, firstname, middlename, lastname, birthdate, email, password) VALUES ('sunman', 'melih', 'cabir', 'sunman', '1999-09-30', 'm.sunman@yandex.com', 'qwerasdfzxcv');
-# INSERT INTO users (username, firstname, lastname, email, password) VALUES ('admin', 'admin', 'admin', 'admin@admin.com', 'passworddd123512');
-
-
 conn.commit()
-
